2328c124a5206c1c199051124b46bcd321eb2ff5
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2023-03-26 18:52:53 +0200

    added current date in login screen

--- app/src/main/java/com/example/diaryapp/MainActivity.java
+++ app/src/main/java/com/example/diaryapp/MainActivity.java
@@ -2,24 +2,37 @@ package com.example.diaryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     Button loginButton;
     EditText et1;
     EditText et2;
+    TextView date;
+    Date d = new Date();
+    CharSequence s  = DateFormat.format("MMMM d, yyyy ", d.getTime());
+    String day = LocalDate.now().getDayOfWeek().name();
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         et1 = (EditText) findViewById(R.id.login_email);
         et2 = (EditText) findViewById(R.id.login_pass);
+        date = (TextView) this.findViewById(R.id.date);
+        date.setText(day + " - " +s);
 
         //Opening Database
         DiaryDatabaseManager dbManager = new DiaryDatabaseManager(getApplicationContext());
