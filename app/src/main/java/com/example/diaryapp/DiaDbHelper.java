07760ac62d8a41d9861c593808dca9190ecea2ed
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2023-03-27 01:03:04 +0200

    added entry table to DBHelper

--- app/src/main/java/com/example/diaryapp/DiaDbHelper.java
+++ app/src/main/java/com/example/diaryapp/DiaDbHelper.java
@@ -11,13 +11,19 @@ public class DiaDbHelper extends SQLiteOpenHelper {
 
     // Diary App Tables
     protected static final String DB_USER_TABLE = "user";
-    // protected static final String DB_DIARY_ENTRY_TABLE = "diaryEntry";
+    protected static final String DB_DIARY_ENTRY_TABLE = "diaryEntry";
 
     // User columns
     protected static final String KEY_USER_ID = "uId";
     protected static final String KEY_NAME = "uName";
     protected static final String KEY_EMAIL = "uEmail";
     protected static final String KEY_PASS = "uPass";
+
+    // Diary Entry columns
+    protected static final String KEY_ENTRY_ID = "eId";
+    protected static final String KEY_TITLE = "eTitle";
+    protected static final String KEY_TEXT = "eText";
+    protected static final String KEY_DATE = "eDate";
 
     //Create table queries
     private static final String DB_CREATE_USER_TABLE = "CREATE TABLE " + DB_USER_TABLE + " (" +
@@ -26,6 +32,11 @@ public class DiaDbHelper extends SQLiteOpenHelper {
             KEY_EMAIL + " TEXT NOT NULL UNIQUE, " +
             KEY_PASS + " TEXT NOT NULL);";
 
+    private static final String DB_CREATE_DIARY_ENTRY_TABLE = "CREATE TABLE " + DB_DIARY_ENTRY_TABLE + " (" +
+            KEY_ENTRY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            KEY_TITLE + " TEXT NOT NULL, " +
+            KEY_TEXT + " TEXT NOT NULL, " +
+            KEY_DATE + " DATE NOT NULL);";
 
 
     public DiaDbHelper(Context context) {
@@ -35,11 +46,11 @@ public class DiaDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(DB_CREATE_USER_TABLE);
+        db.execSQL(DB_CREATE_DIARY_ENTRY_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
-
 
 }
