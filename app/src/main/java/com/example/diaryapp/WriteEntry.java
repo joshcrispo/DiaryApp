649388257ea295cf90fcbc47a5dd3cb367c0ac67
Joshua AL Rasbi <joshuacrispo123@gmail.com>
2023-03-27 02:18:34 +0200

    added event handler for writeEntry activity

--- app/src/main/java/com/example/diaryapp/WriteEntry.java
+++ app/src/main/java/com/example/diaryapp/WriteEntry.java
@@ -3,14 +3,25 @@ package com.example.diaryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.LocalDate;
 import java.util.Date;
 
 public class WriteEntry extends AppCompatActivity {
+
+    ImageButton back;
+    ImageButton user;
+    ImageButton photo;
+    ImageButton audio;
+    ImageButton mood;
+    ImageButton save;
+
 
     TextView date;
     Date d = new Date();
@@ -25,5 +36,21 @@ public class WriteEntry extends AppCompatActivity {
 
         date = (TextView) this.findViewById(R.id.entryDate);
         date.setText(day + " - " +s);
+
+        back = (ImageButton) findViewById(R.id.go_back);
+        user = (ImageButton) findViewById(R.id.user_button);
+        photo = (ImageButton) findViewById(R.id.photo);
+        audio = (ImageButton) findViewById(R.id.audio);
+        mood = (ImageButton) findViewById(R.id.mood);
+        save = (ImageButton) findViewById(R.id.save);
+
+        // Event handlers for buttons
+        back.setOnClickListener(view -> startActivity(new Intent(WriteEntry.this, Menu.class)));
+        user.setOnClickListener(view -> Toast.makeText(WriteEntry.this, "Clicked user", Toast.LENGTH_LONG).show());
+        photo.setOnClickListener(view -> Toast.makeText(WriteEntry.this, "Clicked photo", Toast.LENGTH_LONG).show());
+        audio.setOnClickListener(view -> Toast.makeText(WriteEntry.this, "Clicked audio", Toast.LENGTH_LONG).show());
+        mood.setOnClickListener(view -> Toast.makeText(WriteEntry.this, "Clicked mood", Toast.LENGTH_LONG).show());
+        save.setOnClickListener(view -> Toast.makeText(WriteEntry.this, "Clicked save", Toast.LENGTH_LONG).show());
+
     }
 }
